f4107d478bda820bc79440ba813b8dc626193551
Malte Schwitters <malte.schwitters@worldiety.de>
2020-09-02 16:17:36 +0200

    first commit

---
+++ src/app.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    <h1>Example Component</h1>
+    <example-component :start="2"></example-component>
+  </div>
+</template>
+
+<script>
+import ExampleComponent from "./components/example-component";
+
+export default {
+  components: { ExampleComponent },
+  data() {
+    return {
+      
+    }
+  }
+}
+</script>
---
+++ src/components/example-component.vue
@@ -0,0 +1,25 @@
+<template>
+  <button @click="increase">
+    <span>This Button has been clicked {{ current }} times</span>
+  </button>
+</template>
+
+<script>
+  export default {
+    props: {
+      start: Number 
+    },
+
+    data() {
+      return {
+        current: this.start,
+      }
+    },
+
+    methods: {
+      increase() {
+        this.current++;
+      }
+    }
+  }
+</script>
